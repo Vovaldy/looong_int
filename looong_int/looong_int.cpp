fd3ea8f87662c5e9c22bc1b69aa423db831b79e3
Vovaldy <raek06@mail.ru>
2023-11-03 14:13:55 +0300

    Добавьте файлы проекта.

---
+++ looong_int/looong_int.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "big_integer.hpp"
+using namespace std;
+
+
+int main() {
+	big_integer bi("2"), bi2 = 100;
+	//cout << bi.pow(1000) << endl << endl;
+	long n;
+	cout << "Введите n" << endl;
+	cin >> n;
+	
+	cout << "2^n - 1" << endl;
+	cout << bi.pow(n) - 1 << endl << endl;
+	cout << "2^(n-1) + 1" << endl;
+	cout << bi.pow(n-1) + 1 << endl << endl;
+}
